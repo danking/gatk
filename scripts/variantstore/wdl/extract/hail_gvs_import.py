1219a589a592648456a97d2c09583f91690a6453
Dan King <daniel.zidan.king@gmail.com>
2023-11-21 16:52:28 -0500

    Update hail_gvs_import.py

--- scripts/variantstore/wdl/extract/hail_gvs_import.py
+++ scripts/variantstore/wdl/extract/hail_gvs_import.py
@@ -20,6 +20,7 @@ def create_vds(argsfn, vds_path, references_path, temp_path, use_classic_vqsr):
     from hail.utils.java import Env
 
     hl.init(tmp_dir=f'{temp_path}/hail_tmp_general')
+    hl._set_flags(use_new_shuffle='1')
 
     rg38 = hl.get_reference('GRCh38')
     rg38.add_sequence(f'{references_path}/Homo_sapiens_assembly38.fasta.gz',
